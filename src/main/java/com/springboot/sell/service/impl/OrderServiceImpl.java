1ac7ab048ee7351439f164630230796f7ff6fb88
jerry <jm90_yang@163.com>
2018-12-03 18:06:41 +0800

    创建订单

--- src/main/java/com/springboot/sell/service/impl/OrderServiceImpl.java
+++ src/main/java/com/springboot/sell/service/impl/OrderServiceImpl.java
@@ -1,10 +1,29 @@
 package com.springboot.sell.service.impl;
 
+import com.springboot.sell.dto.CartDTO;
 import com.springboot.sell.dto.OrderDTO;
+import com.springboot.sell.enums.OrderStatusEnum;
+import com.springboot.sell.enums.PayStatusEnum;
+import com.springboot.sell.enums.ResultEnum;
+import com.springboot.sell.exception.SellException;
+import com.springboot.sell.model.OrderDetail;
+import com.springboot.sell.model.OrderMaster;
+import com.springboot.sell.model.ProductInfo;
+import com.springboot.sell.repository.OrderDetailRepository;
+import com.springboot.sell.repository.OrderMasterRepository;
 import com.springboot.sell.service.OrderService;
+import com.springboot.sell.service.ProductService;
+import com.springboot.sell.utils.KeyUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @ Author     ：jmyang
@@ -15,9 +34,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
     @Override
     public OrderDTO create(OrderDTO orderDTO) {
-        return null;
+
+        String orderId = KeyUtil.genUniqueKey();
+        BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+        //1. 查询商品（数量, 价格）
+        List<OrderDetail> orderDetailList = orderDTO.getOrderDetailList();
+        for (OrderDetail orderDetail : orderDetailList) {
+            ProductInfo productInfo = productService.findOne(orderDetail.getProductId());
+            if (productInfo == null){
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            //2. 计算订单总价
+            orderAmount = productInfo.getProductPrice().multiply(new BigDecimal(orderDetail.getProductQuantity())).add(orderAmount);
+            //订单详情入库
+            //订单详情入库
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo, orderDetail);
+            orderDetailRepository.save(orderDetail);
+        }
+
+        //3. 写入订单数据库（orderMaster和orderDetail）
+        OrderMaster orderMaster = new OrderMaster();
+        orderDTO.setOrderId(orderId);
+        BeanUtils.copyProperties(orderDTO, orderMaster);
+        orderMaster.setOrderAmount(orderAmount);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterRepository.save(orderMaster);
+
+        //4. 扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e ->
+                new CartDTO(e.getProductId(), e.getProductQuantity())
+        ).collect(Collectors.toList());
+        productService.decreaseStock(cartDTOList);
+        return orderDTO;
     }
 
     @Override
